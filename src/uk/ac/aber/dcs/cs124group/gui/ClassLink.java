c23fae19f340208a6f92d1986d743ffbf573a161
Samuel Sherar <s.sherar@live.com>
2012-02-19 23:45:48 +0000

    Expanded ClassRectangles and added basic functionality of ClassLink

---
+++ src/uk/ac/aber/dcs/cs124group/gui/ClassLink.java
@@ -0,0 +1,54 @@
+package uk.ac.aber.dcs.cs124group.gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import uk.ac.aber.dcs.cs124group.model.*;
+
+public class ClassLink extends ModelLink {
+	
+	private ClassRectangle currentClass;
+	
+	public void ClassLink(ClassRectangle c) {
+		this.currentClass = c;
+	}
+	
+	public int getWidth() {
+		return this.currentClass.getSize().width;
+	}
+	
+	public int getHeight() {
+		return this.currentClass.getSize().height;
+	}
+	
+	public Point getPosition() {
+		return this.currentClass.getPosition();
+	}
+	
+	public String[] getDataFields() {
+		//Placesetter...
+		return new String[1];
+	}
+	
+	public String[] getMethods() {
+		return new String[1];
+	}
+
+	@Override
+	public void processActionEvent(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void processMouseEvent(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void processKeyEvent(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/uk/ac/aber/dcs/cs124group/model/ClassRectangle.java
+++ src/uk/ac/aber/dcs/cs124group/model/ClassRectangle.java
@@ -1,10 +1,16 @@
 package uk.ac.aber.dcs.cs124group.model;
 
-import java.awt.Point;
+import java.awt.*;
 
 public class ClassRectangle extends DocumentElement {
 
 	private static final long serialVersionUID = 249568855144662119L;
+	
+	private String name;
+	private Dimension size;
+	private ClassRectangle superClass = null;
+	private boolean isAbstract = false;
+	private boolean isFinal = false;
 
 	public ClassRectangle(Point p) {
 		position = p;
@@ -12,8 +18,25 @@ public class ClassRectangle extends DocumentElement {
 
 	@Override
 	public void move(Point newPos) {
-		
-
+		position = newPos;
+		//act upon this new information accordingly...
+	}
+	
+	public void resize(Dimension newSize) {
+		this.size = newSize;
+		//act upon this new information accordingly...
+	}
+	
+	public Dimension getSize() {
+		return this.size;
+	}
+	
+	public void setSuperClass(ClassRectangle c) {
+		this.superClass = c;
+	}
+	
+	public ClassRectangle getSuperClass() {
+		return this.superClass;
 	}
 
 }
