d9d8e01a1c69b83e715f5c161f488ab7f54ad726
Samuel Sherar <s.sherar@live.com>
2012-02-19 16:54:06 +0000

    More skeleton classes for the GUI package.

---
+++ src/uk/ac/aber/dcs/cs124group/gui/MenuBar.java
@@ -0,0 +1,11 @@
+package uk.ac.aber.dcs.cs124group.gui;
+
+import javax.swing.*;
+import java.util.*;
+
+public class MenuBar extends JMenuBar {
+	private LinkedList<JMenuItem> file;
+	private LinkedList<JMenuItem> export;
+	private LinkedList<JMenuItem> edit;
+	
+}
---
+++ src/uk/ac/aber/dcs/cs124group/gui/SideBar.java
@@ -0,0 +1,13 @@
+package uk.ac.aber.dcs.cs124group.gui;
+
+import javax.swing.*;
+
+public class SideBar extends JPanel {
+	
+	private JButton newClass;
+	private JButton newRelationship;
+	
+	void SideBar() {
+		
+	}
+}
