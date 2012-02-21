0f9007a523720d83bca772b7f1e0609e8dbde3b4
Daniel Maly <maly.dan@gmail.com>
2012-02-21 18:33:22 +0000

    Status bar text properly aligned

--- src/uk/ac/aber/dcs/cs124group/gui/StatusBar.java
+++ src/uk/ac/aber/dcs/cs124group/gui/StatusBar.java
@@ -9,17 +9,18 @@ public class StatusBar extends JPanel {
 	private JLabel textLabel = new JLabel();
 	
 	public StatusBar() {
-		
+		setLayout(new GridLayout(1,1,5,0));	
 		this.setPreferredSize(new Dimension(0,20));
 		add(textLabel);
+				
 	}
 	
 	public void setText(String text) {
-		textLabel.setText(text);
+		textLabel.setText(" " + text);
 	}
 	
 	public String getText() {
-		return textLabel.getText();
+		return textLabel.getText().trim();
 	}
 	
 	
