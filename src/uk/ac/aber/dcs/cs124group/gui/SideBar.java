d0ff593124ed60fe2f2f3268cf0574d42c40aaf4
Samuel B Sherar <s.sherar@live.com>
2012-02-21 11:56:05 +0000

    Finished padding buttons

--- src/uk/ac/aber/dcs/cs124group/gui/SideBar.java
+++ src/uk/ac/aber/dcs/cs124group/gui/SideBar.java
@@ -5,6 +5,9 @@ import java.awt.*;
 
 public class SideBar extends JPanel {
 	
+	private final int SIDE_PADDING = 20;
+	private final int TOP_PADDING = 20;
+	private final int VERT_PADDING = 5;
 	private JButton newClass, newRelationship, newTextLabel, export;
 	private SpringLayout layout;
 	
@@ -16,18 +19,24 @@ public class SideBar extends JPanel {
 		
 		newClass = new JButton("New Class");
 		newRelationship = new JButton("New Relationship");
-		newTextLabel = new JButton("New Textlabel");
+		newTextLabel = new JButton("New label");
 		export = new JButton("Export to Java...");
 		
 		newClass.setPreferredSize(newRelationship.getPreferredSize());
 		newTextLabel.setPreferredSize(newRelationship.getPreferredSize());
 		export.setPreferredSize(newRelationship.getPreferredSize());
 		
-		layout.putConstraint(SpringLayout.WEST, newClass, 20, SpringLayout.NORTH, this);
-		layout.putConstraint(SpringLayout.NORTH, newClass, 20, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.WEST, newClass, SIDE_PADDING, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.NORTH, newClass, TOP_PADDING, SpringLayout.NORTH, this);
 		
-		layout.putConstraint(SpringLayout.WEST, newRelationship, 20, SpringLayout.NORTH, this);
-		layout.putConstraint(SpringLayout.NORTH, newRelationship, 5, SpringLayout.SOUTH, newClass);
+		layout.putConstraint(SpringLayout.WEST, newRelationship, SIDE_PADDING, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.NORTH, newRelationship, VERT_PADDING, SpringLayout.SOUTH, newClass);
+		
+		layout.putConstraint(SpringLayout.WEST, newTextLabel, SIDE_PADDING, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.NORTH, newTextLabel, VERT_PADDING, SpringLayout.SOUTH, newRelationship);
+		
+		layout.putConstraint(SpringLayout.WEST, export, SIDE_PADDING, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.NORTH, export, VERT_PADDING, SpringLayout.SOUTH, newTextLabel);
 		
 		this.add(newClass);
 		this.add(newRelationship);
