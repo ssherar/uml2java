f7b0dbd5e001d9845e594415d9eb23533fa963f3
Daniel Maly <maly.dan@gmail.com>
2012-02-22 01:27:31 +0000

    Even more rendering quality

--- src/uk/ac/aber/dcs/cs124group/gui/Canvas.java
+++ src/uk/ac/aber/dcs/cs124group/gui/Canvas.java
@@ -40,6 +40,16 @@ public class Canvas extends JPanel {
 		g.setRenderingHint(
 		        RenderingHints.KEY_TEXT_ANTIALIASING,
 		        RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+		g.setRenderingHint(
+				RenderingHints.KEY_ANTIALIASING, 
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		g.setRenderingHint(
+				RenderingHints.KEY_FRACTIONALMETRICS,
+				RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+		g.setRenderingHint(
+				RenderingHints.KEY_RENDERING,
+				RenderingHints.VALUE_RENDER_QUALITY);
+				
 		
 		ArrayList<DocumentElement> elements = new ArrayList<DocumentElement>();
 		try {
